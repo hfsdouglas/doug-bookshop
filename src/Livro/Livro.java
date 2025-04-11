fa72f86d6a9f5e72adef9a78d1b7d24316e0bb98
Douglas Faria <hfsdouglas@gmail.com>
2025-04-10 21:33:46 -0300

    feat: add book package

---
+++ src/Livro/Livro.java
@@ -0,0 +1,62 @@
+package Livro;
+
+import Autor.Autor;
+
+import java.time.LocalDateTime;
+
+public class Livro {
+    private Integer id;
+    private String titulo;
+    private Autor autor;
+    private Boolean disponivel;
+    private LocalDateTime dataCadastro;
+    private LocalDateTime dataAtualizacao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public Boolean getDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(Boolean disponivel) {
+        this.disponivel = disponivel;
+    }
+
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public LocalDateTime getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(LocalDateTime dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+}
