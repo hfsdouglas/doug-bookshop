eabb122b38daea2343a627203db99d002044eddd
Douglas Faria <hfsdouglas@gmail.com>
2025-04-10 21:31:31 -0300

    feat: add author package

---
+++ src/Autor/Autor.java
@@ -0,0 +1,33 @@
+package Autor;
+
+import java.time.LocalDateTime;
+
+public class Autor {
+    private Integer id;
+    private String nome;
+    private LocalDateTime dataNascimento;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDateTime getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDateTime dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
